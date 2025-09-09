18b37c4fe5d6c8bf44b491d0944c46ca76a43c95
sonugupta81 <sonuguptak2005@gmail.com>
2025-09-09 23:17:06 +0530

    new

--- app/component/Hero.js
+++ app/component/Hero.js
@@ -1,33 +1,75 @@
 import React from "react";
-import portfolio from "../assets/images/portfolio.jpg";
+import profile from "../assets/images/profile.jpg";
+import picture from "../assets/images/picture.png";
 import Image from "next/image";
 import Flower from "../assets/images/flower.png";
+import Newdelhi from "../assets/images/newdelhi.jpg";
+import { ArrowUpRight } from "lucide-react";
 
 const Hero = () => {
   return (
     <div className="px-10 pt-10 flex gap-8">
-      <div className="w-2/3 flex gap-6  ">
-        <div className="w-2/3 flex flex-col gap-6 bg-rose-100 rounded-xl p-10">
-          <div className="flex justify-end ">
-            <Image src={Flower} alt="Portfolio" height={100} width={100} />
-          </div>
+      <div className="w-2/3 flex flex-col gap-6">
+        <div className="flex gap-6">
+          <div className="w-2/3 flex flex-col gap-6 bg-rose-100 rounded-xl p-10">
+            <div className="flex justify-end ">
+              <Image src={Flower} alt="flower" height={100} width={100} />
+            </div>
 
-          <p className="w-4/6 text-5xl font-bold w-full">
-            Artist Redefining <span className="italic">Architecture</span> with
-            Al-Driven Design
-          </p>
+            <p className="w-4/6 text-5xl font-bold w-full pt-15">
+              Artist Redefining <span className="italic">Architecture</span>{" "}
+              with Al-Driven Design
+            </p>
+          </div>
+          <div className="w-2/6 ">
+            <Image
+              className="rounded-xl"
+              src={profile}
+              alt="Portfolio"
+              height={400}
+              width={300}
+            />
+          </div>
         </div>
-        <div className="w-2/6 ">
-          <Image
-            className="rounded-xl"
-            src={portfolio}
-            alt="Portfolio"
-            height={400}
-            width={300}
-          />
+        <div className="w-full flex gap-6  ">
+          <div className="w-1/2 bg-rose-100 rounded-xl pt-10">
+          <div className=" px-12 pb-15">
+            <Image src={picture} alt="Portfolio" height={50} width={50} />
+          </div>
+          <p className="flex px-12 text-2xl font-thin pb-10">Julia Huang is an innovative Al artist, renowned for blending cutting-edge technology with creative expression. Based in LA,
+             she crafts unique digital art experiences accessible globally.</p>
+          </div>
+          <div className="w-1/2 bg-rose-300 rounded-xl pt-10">
+          <div className=" flex justify-end px-8"><ArrowUpRight/></div>
+          <p className=" px-10 ">Have Some Qustion?</p>
+          <p className=" text-6xl pt-50  px-6">Contact me</p>
+          </div>
         </div>
       </div>
-      <div className="w-1/3 flex bg-rose-100 rounded-xl">India</div>
+      <div className="w-1/3 flex flex-col bg-rose-100 rounded-xl">
+      <div className=" px-8 pt-6  ">
+        <div className="flex justify-end "><ArrowUpRight/></div>
+        <p className="text-2xl pb-5">Delhi</p> 
+        <div className="pb-6">
+        <Image className="rounded-xl "
+         src={Newdelhi}
+         alt="Portfolio" 
+         height={300} 
+         width={400}
+        />
+        </div>
+        <div>
+        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Elara</p>
+        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Verve</p>
+        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Zephyr</p>
+        </div>
+         <div className="flex gap-15 justify-center pt-25 ">
+         <p className="font-bold  font-thin text-sm">INSTAGRAM</p>
+         <p className="font-bold  font-thin text-sm">TWITTER</p>
+         <p className="font-bold  font-thin text-sm"> LINKEDIN</p>
+         </div>
+      </div>
+      </div>
     </div>
   );
 };
