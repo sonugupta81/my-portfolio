c6ad5d9b33a4441dc9640a6f2f85b2743fdadb95
sonugupta81 <sonuguptak2005@gmail.com>
2025-09-11 19:21:51 +0530

    responsive mobile

--- app/component/Hero.js
+++ app/component/Hero.js
@@ -8,67 +8,86 @@ import { ArrowUpRight } from "lucide-react";
 
 const Hero = () => {
   return (
-    <div className="px-10 pt-10 flex gap-8">
-      <div className="w-2/3 flex flex-col gap-6">
-        <div className="flex gap-6">
-          <div className="w-2/3 flex flex-col gap-6 bg-rose-100 rounded-xl p-10">
+    <div className="px-4 md:px-10 py-10 flex flex-col md:flex-row gap-8">
+      <div className="md:w-2/3 flex flex-col gap-6">
+        <div className="flex flex-col md:flex-row gap-6">
+          <div className=" md:w-2/3 flex flex-col gap-6 bg-rose-100 rounded-xl p-10">
             <div className="flex justify-end ">
               <Image src={Flower} alt="flower" height={100} width={100} />
             </div>
 
-            <p className="w-4/6 text-5xl font-bold w-full pt-15">
+            <p className="md:w-4/6 text-5xl font-bold w-full pt-50">
               Artist Redefining <span className="italic">Architecture</span>{" "}
               with Al-Driven Design
             </p>
           </div>
-          <div className="w-2/6 ">
+          <div className="md:w-2/6  flex justify-center ">
             <Image
               className="rounded-xl"
               src={profile}
               alt="Portfolio"
               height={400}
-              width={300}
+              width={500}
             />
           </div>
         </div>
-        <div className="w-full flex gap-6  ">
-          <div className="w-1/2 bg-rose-100 rounded-xl pt-10">
-          <div className=" px-12 pb-15">
-            <Image src={picture} alt="Portfolio" height={50} width={50} />
+        <div className="w-full flex flex-col md:flex-row gap-6  ">
+          <div className="md:w-1/2 bg-rose-100 rounded-xl pt-10">
+            <div className=" px-12 pb-15">
+              <Image src={picture} alt="Portfolio" height={50} width={50} />
+            </div>
+            <p className="flex px-12 text-2xl font-thin pb-10">
+              Julia Huang is an innovative Al artist, renowned for blending
+              cutting-edge technology with creative expression. Based in LA, she
+              crafts unique digital art experiences accessible globally.
+            </p>
           </div>
-          <p className="flex px-12 text-2xl font-thin pb-10">Julia Huang is an innovative Al artist, renowned for blending cutting-edge technology with creative expression. Based in LA,
-             she crafts unique digital art experiences accessible globally.</p>
-          </div>
-          <div className="w-1/2 bg-rose-300 rounded-xl pt-10">
-          <div className=" flex justify-end px-8"><ArrowUpRight/></div>
-          <p className=" px-10 ">Have Some Qustion?</p>
-          <p className=" text-6xl pt-50  px-6">Contact me</p>
+          <div className="md:w-1/2 bg-rose-300 rounded-xl pb-15 pt-20">
+            <div className=" flex justify-end px-8">
+              <ArrowUpRight />
+            </div>
+            <p className=" px-10 ">Have Some Qustion?</p>
+            <p className=" text-6xl pt-50  px-6">Contact me</p>
           </div>
         </div>
       </div>
-      <div className="w-1/3 flex flex-col bg-rose-100 rounded-xl">
-      <div className=" px-8 pt-6  ">
-        <div className="flex justify-end "><ArrowUpRight/></div>
-        <p className="text-2xl pb-5">Delhi</p> 
-        <div className="pb-6">
-        <Image className="rounded-xl "
-         src={Newdelhi}
-         alt="Portfolio" 
-         height={300} 
-         width={400}
-        />
+      <div className="md:w-1/3  flex flex-col rounded-xl">
+        <div className="  ">
+          <div className="bg-rose-100 p-8 rounded-xl">
+            <div className="flex justify-between items-center pb-4">
+              <p className="text-2xl">Delhi</p>
+              <ArrowUpRight />
+            </div>
+            <div className="pb-10">
+              <Image
+                className="rounded-xl "
+                src={Newdelhi}
+                alt="Portfolio"
+                height={700}
+                width={700}
+              />
+            </div>
+            <div>
+              <p className="text-3xl border-t-3 border-rose-300 pt-10 pb-10">
+                {" "}
+                Elara
+              </p>
+              <p className="text-3xl border-t-3 border-rose-300 pt-10 pb-10">
+                {" "}
+                Verve
+              </p>
+              <p className="text-3xl border-t-3 border-rose-300 pt-10 pb-10">
+                {" "}
+                Zephyr
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-15 justify-center   bg-rose-100 rounded-xl mt-15 py-19">
+            <p className="font-bold  font-thin text-sm">INSTAGRAM</p>
+            <p className="font-bold  font-thin text-sm">TWITTER</p>
+            <p className="font-bold  font-thin text-sm"> LINKEDIN</p>
+          </div>
         </div>
-        <div>
-        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Elara</p>
-        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Verve</p>
-        <p className="text-3xl border-t-3 border-rose-300 pt-8 pb-8"> Zephyr</p>
-        </div>
-         <div className="flex gap-15 justify-center pt-25 ">
-         <p className="font-bold  font-thin text-sm">INSTAGRAM</p>
-         <p className="font-bold  font-thin text-sm">TWITTER</p>
-         <p className="font-bold  font-thin text-sm"> LINKEDIN</p>
-         </div>
-      </div>
       </div>
     </div>
   );
--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 
-    const Navbar = () => {
-      return (
-        <div>
-          <div className="px-10 pt-10 ">
-        <div className="bg-rose-100 px-6 py-10 rounded-3xl flex justify-between  ">
+const Navbar = () => {
+  return (
+    <div>
+      <div className="px-4 md:px-10 pt-10 ">
+        <div className="bg-rose-100 px-6 py-10 md:py-15 rounded-3xl flex flex-col md:flex-row justify-between  ">
           <p className="text-xl">
             <span className="italic font-[10]">SONU</span>{" "}
             <span className="font-bold">GUPTA</span>{" "}
           </p>
-          <div className=" flex gap-15">
+          <div className=" flex gap-15 pt-8 md:pt-0">
             <p className="font-bold  font-thin text-sm">PROJECTS</p>
             <p className="font-bold  font-thin text-sm">ABOUT</p>
             <p className="font-bold font-thin text-sm">CONTACT</p>
           </div>
         </div>
       </div>
-        </div>
-      );
-    };
+    </div>
+  );
+};
 
-    export default Navbar;
+export default Navbar;
